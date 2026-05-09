810ec0e54bf970c1c24403613a45a9ca745a26f1
iSamuel <okesunday503@gmail.com>
2026-05-09 17:26:05 +0100

    Update sitemap URL format in sitemap-index.xml.ts

--- src/pages/sitemap-index.xml.ts
+++ src/pages/sitemap-index.xml.ts
@@ -1,12 +1,12 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ site }) => {
-  const siteUrl = site?.toString() ?? "";
+  const siteUrl = site?.toString().replace(/\/$/, "") ?? "";
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
-    <loc>${siteUrl}sitemap-0.xml</loc>
+    <loc>${siteUrl}/sitemap.xml</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
   </sitemap>
 </sitemapindex>`;
