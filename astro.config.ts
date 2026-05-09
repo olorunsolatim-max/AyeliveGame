46bf4b8db2f3ee115145492b7f3a4c191bf03b12
iSamuel <okesunday503@gmail.com>
2026-05-09 17:16:23 +0100

    Correct site URL and format file
    Fix site URL formatting and add missing newline at end of file.

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: https://isamueldev.vercel.app/",
+  site: "https://isamueldev.vercel.app",
   integrations: [
     tailwind(),
     sitemap({
