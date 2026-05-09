ab9febac7e0ea3b7c67b6fc095030ccd9de2f4d4
iSamuel <okesunday503@gmail.com>
2026-05-09 17:22:39 +0100

    Remove sitemap integration from astro.config.ts
    Removed sitemap integration from Astro config.

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://isamueldev.vercel.app",
   integrations: [
     tailwind(),
-    sitemap({
-      filter: (page) =>
-        !page.includes("/search-index") &&
-        !page.includes("/404"),
-      changefreq: "weekly",
-      priority: 0.7,
-    }),
   ],
   markdown: {
     shikiConfig: {
