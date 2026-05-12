61c07f2da104f70f80724af7e204259b1d276ad6
iSamuel <okesunday503@gmail.com>
2026-05-12 09:02:34 +0100

    Add sitemap-images.xml to sitemap index

--- src/pages/sitemap-index.xml.ts
+++ src/pages/sitemap-index.xml.ts
@@ -1,12 +1,16 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async () => {
-  const siteUrl = "https://isamueldev.vercel.app";
+  const siteUrl = "https://isamueldev.vercel.app/";
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
     <loc>${siteUrl}/sitemap.xml</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${siteUrl}/sitemap-images.xml</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
   </sitemap>
 </sitemapindex>`;
