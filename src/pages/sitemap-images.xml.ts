52a59863514bf06469fa26a8939e83375bcef928
iSamuel <okesunday503@gmail.com>
2026-05-12 09:01:17 +0100

    Add sitemap generation for blog images

---
+++ src/pages/sitemap-images.xml.ts
@@ -0,0 +1,35 @@
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+
+export const GET: APIRoute = async () => {
+  const siteUrl = "https://isamueldev.vercel.app/";
+
+  const posts = await getCollection("blog", ({ data }) => !data.draft);
+
+  const postsWithImages = posts.filter(post => post.data.cover);
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+${postsWithImages.map(post => {
+  const imageUrl = post.data.cover?.startsWith("http")
+    ? post.data.cover
+    : `${siteUrl}${post.data.cover}`;
+
+  return `  <url>
+    <loc>${siteUrl}/blog/${post.slug}/</loc>
+    <image:image>
+      <image:loc>${imageUrl}</image:loc>
+      <image:title>${post.data.title.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")}</image:title>
+      <image:caption>${post.data.description.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")}</image:caption>
+    </image:image>
+  </url>`;
+}).join("\n")}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+};
